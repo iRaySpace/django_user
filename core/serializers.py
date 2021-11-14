99da4842d286aac214d1f162df2b6de82b29ab03
iRaySpace <irayspacii@gmail.com>
2021-11-14 23:15:36 +0800

    initial commit

---
+++ core/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+    
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            validated_data.get('username'),
+            validated_data.get('password'),
+        )
+        return user
---
+++ core/views.py
@@ -0,0 +1,14 @@
+from rest_framework import generics
+from rest_framework.response import Response
+from .serializers import RegisterSerializer
+
+
+class RegisterAPIView(generics.GenericAPIView):
+    serializer_class = RegisterSerializer
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response({
+            'user': serializer.data,
+        })
